1f64e7d29dfcd148d7f330f00bfb137caf0d85f4
fed-gren <deveb22@gmail.com>
2019-05-27 00:09:44 +0900

    fix: mofify bio contents
    - bio 내용 수정

--- src/components/bio.js
+++ src/components/bio.js
@@ -16,7 +16,7 @@ function Bio() {
     <StaticQuery
       query={bioQuery}
       render={data => {
-        const { author, social } = data.site.siteMetadata
+        const { author } = data.site.siteMetadata
         return (
           <div
             style={{
@@ -38,11 +38,11 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong>
               {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+              <br/>
+              <a href={`https://github.com/fed-gren`} target="_blank" rel="noopener noreferrer">
+                github
               </a>
             </p>
           </div>
